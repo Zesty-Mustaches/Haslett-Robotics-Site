bab8329d3e8f587ca49acbaec82cb3dab77c8796
Sartaj GIll <53071012+Sartaj21@users.noreply.github.com>
2021-07-17 21:19:10 -0400

    Put in team contents for header

---
+++ hasslet/src/Header.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import './Header.css'
+
+
+const Header = () => {
+    return (
+        <div className = 'header'>
+            <div className = 'header_logo'>
+                <div className="header_center">
+                    <button class="about_teams">
+                        Teams
+                        <i class="fa fa-caret-down"></i>
+                    </button>
+                    <div class = "about_teams_content">
+                        <button class="vrc_teams">
+                            VRC Teams
+                            <i class="fa fa-caret-down"></i>
+                        </button>
+                        <button class="iq_team">
+                            IQ Teams
+                            <i class="fa fa-caret-down"></i>
+                        </button>
+                    </div>
+                    <div class="iq_indv_teams">
+                        <a href=".">
+                            <p>10452A</p>
+                        </a>
+                        <a href=".">
+                            <p>10452W</p>
+                        </a>
+                        <a href=".">
+                            <p>10452Q</p>
+                        </a>
+                        <a href=".">
+                            <p>10452</p>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
